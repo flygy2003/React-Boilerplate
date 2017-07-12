cc2581e1f18cbbb09c1fb384af1a755f6e853417
Neekon Saadat <flygy2003@users.noreply.github.com>
2017-07-12 09:05:32 -0700

    Let's see some change around here!

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,14 +1,12 @@
 import React from "react"
 import Toolbar from "./Toolbar.jsx"
-import Cards from "./Cards.jsx"
 
 export default class Layout extends React.Component {
   render() {
     return (
       <div>
-        <Toolbar/>
         <body>
-          <Cards/>
+          <Toolbar/>
         </body>
       </div>
     );
--- src/js/components/Toolbar.jsx
+++ src/js/components/Toolbar.jsx
@@ -1,38 +1,20 @@
-import React, {Component} from "react"
+import React, { Component } from 'react'
 
 class Toolbar extends Component {
   render() {
-    return (
+    return(
       <div>
-        <div className="toolbar">
-          <ReactLogo/>
-          <div className='toolbar-links'>
-            {[
-              {
-                name: "Settings",
-                link: '/settings'
-              }
-            ].map((item, i) => {
-              return <a href={item.link} to={item.link} className="toolbar-links-a">
-                {item.name}</a>
-           })}
-          </div>
-        </div>
+        <Logo/>
+        <div id="footer"/>
       </div>
-    );
+    )
   }
 }
 
-class ReactLogo extends Component {
+class Logo extends Component {
   render() {
     return(
-      <div className="viewBox1">
-        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 319.25 318.5">
-          <title>keystock</title>
-          <polygon class="cls-1" points="151.78 159.05 303.35 318 0.5 318 0.5 0.5 318 0.5 151.78 159.05"/>
-          <line class="cls-2" x1="318" y1="0.5" x2="0.5" y2="303.35"/>
-        </svg>
-      </div>
+      <img src="../static/react.png" id="logo"/>
     )
   }
 }
