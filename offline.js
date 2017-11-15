4a351451948f4edfec1fe284ec06562664268648
Neekon Saadat <neekonsu@gmail.com>
2017-11-14 23:11:06 -0800

    simplified database

--- offline.js
+++ offline.js
@@ -3,47 +3,46 @@ const newWebpackMiddle = require('webpack-express-middleware'),
   express = require('express'),
   colors = require('colors'),
   config = require('./webpack.config.js'),
-  app = express()
-  io = require('socket.io')(80),
+  app = express(),
+  io = require('socket.io')(8080),
   ip = require('ip'),
+  sw = require('rpi-gpio'),
+  webpack = newWebpackMiddle(compiler, config)
+  webpack(app)
 
-app.set('port', process.env.PORT || 80);
-
-const webpack = newWebpackMiddle(compiler, config);
-webpack(app)
-var init = {
-  "all": false,
-  "sAtrium": false,
-  "sDiningRoom": false,
-  "sFamilyRoom": false,
-  "sGallery": false,
-  "sGallery2": false,
-  "sGallery3": false,
-  "sGuestBathroom": true,
-  "sHomeworkRoom": false,
-  "sLibrary": false,
-  "sLivingRoom": true,
-  "sLivingRoom 2": false,
-  "sLivingRoom 3": true,
-  "sLivingRoom3": false,
-  "sMasterBathrooms": false,
-  "sMasterBedroom": false,
-  "sNeekon'sBedroom": false,
-  "sOffice": true,
-  "sOfficeBathroom": true,
-  "sRyan'sBathroom": false,
-  "sRyan'sBedroom": false,
-  "skitchen": true
-}
-
-io.on('connection', (socket) => {
-  socket.on('switch', function (data) {
-    console.log(data);
-  })
-  socket.on('all', function (data) {
-    console.log(data);
-  })
+  var rooms = {
+    'kitchen': [7, 40, 36, 16],
+    'livingroom': 31,
+    'livingroom2': 15,
+    'livingroom3': 38,
+    'gallery': 35,
+    'gallery2': 29,
+    'gallery3': 12,
+    'office': [22, 32],
+    'neekonsbedroom': 18,
+    'homeworkroom': 33,
+    'diningroom': 13,
+    'atrium': 11
+  }
+  function switchSingle(id, state) {
+    sw.setup(rooms.id, sw.DIR_OUT, () => {
+      sw.write(rooms.id, state, (err) => {
+        if (err) {
+          throw err
+        }
+      })
+    })
+  }
+  
+  io.on('connection', (socket) => {
+    socket.on('switch', (data) => {
+      console.log(data)
+    })
+    socket.on('all', (data) => {
+      console.log(data)
+    })
 })
+app.set('port', process.env.PORT || 80)
 
 app.use('*', express.static(__dirname + '/src/static'))
 
---
+++ sandbox.js
@@ -0,0 +1,18 @@
+  var rooms = {
+    'kitchen': [7, 40, 36, 16],
+    'livingroom': 31,
+    'livingroom2': 15,
+    'livingroom3': 38,
+    'gallery': 35,
+    'gallery2': 29,
+    'gallery3': 12,
+    'office': [22, 32],
+    'neekonsbedroom': 18,
+    'homeworkroom': 33,
+    'diningroom': 13,
+    'atrium': 11
+  }
+function switchOne(id) {
+  console.log(rooms.id)
+}
+switchOne('office')
