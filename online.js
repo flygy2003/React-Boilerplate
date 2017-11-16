5a82553de1891e1c2c5f4fc4b36c0d438bab89c4
Neekon Saadat <neekonsu@gmail.com>
2017-11-15 22:29:31 -0800

    compound functions and more efficient whitespacing

--- online.js
+++ online.js
@@ -11,30 +11,51 @@ const io = require('rpi-gpio'),
   messagingSenderId: "42864256502"
 }
 fb.initializeApp(config)
-
 var db = fb.database().ref()
-var pins = 
-  /*closest to pi*/ [7, 35, 33, 31, 29, 15, 13, 11, 
-  40, 38, 36, 32, 22, 18, 16, 12]/* furthest from pi */
-
-// var pins = [7, 13, 15, 29, 31, 32, 33, 11, 40, 37, 32, 26, 22, 18, 16, 22]
-var rooms = [
-  'skitchen',
-  'sLivingRoom',
-  'sOffice',
-  'sOfficeBathroom',
-  'sNeekon\'sBedroom',
-  'sHomeworkRoom',
-  'sRyan\'sBathroom',
-  'sRyan\'sBedroom',
-  'sDiningRoom',
-  'sGallery',
-  'sGallery2',
-  'sGallery3',
-  'sLivingRoom 2',
-  'sAtrium',
-  'sLivingRoom 3']
-var num_children = 0
+// var pins = 
+//   /*closest to pi*/ [7, 35, 33, 31, 29, 15, 13, 11, 
+//   40, 38, 36, 32, 22, 18, 16, 12]/* furthest from pi */
+var combo = {
+  'skitchen': [7, 40, 36, 16],
+  'sLivingRoom': [31],
+  'sOffice': [22, 32],
+  'sNeekon\'sBedroom': [18],
+  'sHomeworkRoom': [33],
+  'sDiningRoom': [13],
+  'sGallery': [35],
+  'sGallery2': [29],
+  'sGallery3': [12],
+  'sLivingRoom 2': [15],
+  'sAtrium': [11],
+  'sLivingRoom 3': [38]},
+  rooms = [
+    'skitchen',
+    'sLivingRoom',
+    'sOffice',
+    'sNeekon\'sBedroom',
+    'sHomeworkRoom',
+    'sDiningRoom',
+    'sGallery',
+    'sGallery2',
+    'sGallery3',
+    'sLivingRoom 2',
+    'sAtrium',
+    'sLivingRoom 3'],
+    num_children = 0
+    rooms.forEach(item => {
+      io.setup(combo[item], io.DIR_OUT, () => {
+        db.child(`rooms/${item}`)
+        .on('value', (snapshot) => {
+          io.write(combo[item], snapshot.val(), (err) => {
+            if (err) {
+              throw err
+            } else {
+              console.log(`[ ${chalk.blue('i')} ]: ${item} set => ${snapshot.val()}`)
+            }
+          })
+        })
+      })
+  })
 function master_network_if_manager() {
   require('dns').lookup('google.com', function (err) {
     if (err && err.code == "ENOTFOUND") {
@@ -62,197 +83,3 @@ function master_network_if_manager() {
   })
 }
 setInterval(master_network_if_manager, 1000)
-
-io.setup(pins[0], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[0]}`)
-  .on('value', (snapshot) => { //kitchen0
-    io.write(pins[0], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[0]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[1], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[9]}`)
-  .on('value', (snapshot) => { //Gallery9
-    io.write(pins[1], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[9]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-
-io.setup(pins[2], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[5]}`)
-  .on('value', (snapshot) => { // Homework Room5
-    io.write(pins[2], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[5]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[3], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[1]}`)
-    .on('value', (snapshot) => { //Living room1
-    io.write(pins[3], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[1]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[4], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[10]}`)
-    .on('value', (snapshot) => { //Gallery 9:10:11
-    io.write(pins[4], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[10]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[5], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[12]}`)
-    .on('value', (snapshot) => { //Living Room 2:12
-    io.write(pins[5], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[12]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[6], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[8]}`)
-    .on('value', (snapshot) => { //Dining Room8
-    io.write(pins[6], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[8]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[7], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[13]}`)
-    .on('value', (snapshot) => { //Atrium13
-    io.write(pins[7], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[13]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[8], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[0]}`)
-    .on('value', (snapshot) => { //Kitchen0
-    io.write(pins[8], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[0]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[9], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[14]}`)
-    .on('value', (snapshot) => { //Living Room 3:14
-    io.write(pins[9], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[14]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[10], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[0]}`)
-    .on('value', (snapshot) => { //Kitchen Flourescent:0
-    io.write(pins[10], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[0]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[11], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[3]}`)
-    .on('value', (snapshot) => { // Open:none
-    io.write(pins[11], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[3]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[12], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[2]}`)
-    .on('value', (snapshot) => { //Office2
-    io.write(pins[12], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[2]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[13], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[4]}`)
-    .on('value', (snapshot) => { //Neekon's Bedroom4
-    io.write(pins[13], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[4]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[14], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[0]}`)
-    .on('value', (snapshot) => { //Kitchen0
-    io.write(pins[14], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[0]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
-io.setup(pins[15], io.DIR_OUT, () => {
-  db.child(`rooms/${rooms[11]}`)
-    .on('value', (snapshot) => { //Gallery 3:11
-    io.write(pins[15], snapshot.val(), (err) => {
-      if (err) {
-        throw err
-      } else {
-        console.log(`[ ${chalk.blue('i')} ]: ${rooms[11]} set => ${snapshot.val()}`)
-      }
-    })
-  })
-})
--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,34 @@
-  var rooms = {
-    'kitchen': [7, 40, 36, 16],
-    'livingroom': 31,
-    'livingroom2': 15,
-    'livingroom3': 38,
-    'gallery': 35,
-    'gallery2': 29,
-    'gallery3': 12,
-    'office': [22, 32],
-    'neekonsbedroom': 18,
-    'homeworkroom': 33,
-    'diningroom': 13,
-    'atrium': 11
-  }
-function switchOne(id) {
-  console.log(rooms[id])
-}
-switchOne('office')
+var combo = {
+  'skitchen': [7, 40, 36, 16],
+  'sLivingRoom': [31],
+  'sOffice': [22, 32],
+  'sNeekon\'sBedroom': [18],
+  'sHomeworkRoom': [33],
+  'sDiningRoom': [13],
+  'sGallery': [35],
+  'sGallery2': [29],
+  'sGallery3': [12],
+  'sLivingRoom 2': [15],
+  'sAtrium': [11],
+  'sLivingRoom 3': [38]
+},
+  rooms = [
+    'skitchen',
+    'sLivingRoom',
+    'sOffice',
+    'sNeekon\'sBedroom',
+    'sHomeworkRoom',
+    'sDiningRoom',
+    'sGallery',
+    'sGallery2',
+    'sGallery3',
+    'sLivingRoom 2',
+    'sAtrium',
+    'sLivingRoom 3']
+rooms.forEach(item => {
+  console.log(combo[item])
+})
+// function switchOne(id) {
+//   console.log(rooms[id])
+// }
+// switchOne('office')
