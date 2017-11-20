193aa80ada1e581f0982d9acffd53b6bb3035ae2
Neekon Saadat <flygy2003@users.noreply.github.com>
2017-11-19 22:52:47 -0800

    asdasd

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
       {
         test: /\.scss$/,
         loaders: ["style-loader", "css-loader", "sass-loader"]
+      }, 
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
       }
       
     ]
