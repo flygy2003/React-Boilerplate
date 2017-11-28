16c7beed89d19976908a40b53f6101c192660531
Neekon Saadat <neekonsu@gmail.com>
2017-11-27 17:20:07 -0800

    ffeefw

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules|.vscode)/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-0'],
